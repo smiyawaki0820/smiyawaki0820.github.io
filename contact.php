871f2c14f9d2208789ca89bbc2ad229ee789c659
miyawaki <lab.skow.mywk@gmail.com>
2019-09-08 05:39:16 +0900

    hoge

--- contact.php
+++ contact.php
@@ -1,15 +1,15 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="ja">
     <head>
         <meta charset="utf-8">
-        <title>mail Miyawaki</title>
+        <title>Miyawaki Shumpei| 宮脇峻平</title>
     </head>
     <body>
         <?php
 			mb_language("Japanese");
 			mb_internal_encoding("UTF-8");
 
-			$to = "lab.skow.mywk@gmail.com";
+			$to = "skow.kitten@icloud.com";
 			$title = $_POST['name'];
             $content = $_POST['message'];
             $headers = $_POST['email'];
